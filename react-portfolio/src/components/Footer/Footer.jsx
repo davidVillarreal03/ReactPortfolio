50c76bfe1015a4b4eaeeaa9301c595f1ae72eb3a
David Villarreal <djmarch92014@gmail.com>
2024-10-21 12:52:55 -0500

    Portfolio is now updated with header and footer that have proper styling

--- react-portfolio/src/components/Footer/Footer.jsx
+++ react-portfolio/src/components/Footer/Footer.jsx
@@ -1,21 +1,46 @@
 const styles = {
     card: {
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: '50px',
         display: 'flex',
-        justifyContent: 'space-evenly',
-        background: 'blue',   
+        justifyContent: 'space-evenly',   
+        paddingTop: '10px',
+        paddingBottom: '50px',
+        backgroundColor: 'rgba(253 198 163)',
     },
-    item: {
-        fontSize: '20',
-        textColor: 'green'
+    link: {
+        borderRadius: '50px',
+        padding: '10px',
+        // background: '#DCAFBB',
+        fontSize: '20px',
+        color: '#000000',
+        backgroundColor: 'rgba(253 198 163)',
+    },
+    email: {
+      textDecoration: 'bold'  
     }
 }
 
 function Footer() {
     return (
         <footer style={styles.card}>
-                <p style={styles.item}>David Villarreal</p>
-                <p style={styles.item}>davidVillarreal03</p>
+                <p style={styles.email}>
+
+                    <p style={styles.link} >davidVillarr3al@gmail.com</p>   
+                </p>  
+
+               <a href="https://github.com/davidVillarreal03">
+                    <p style={styles.link}>GitHub: davidVillarreal03</p>
+               </a> 
+               <a href="https://stackoverflow.com/users/27916696/david-villarreal">
+                    <p style={styles.link}>Stack Overflow: david-villarreal</p>
+               </a> 
+        
         </footer>
+
     );
 }
 
--- react-portfolio/src/components/Navigation/Navigation.jsx
+++ react-portfolio/src/components/Navigation/Navigation.jsx
@@ -1,22 +1,39 @@
 import { Link, useLocation } from 'react-router-dom';
 import './Navigation.css';
-
+const styles = {
+    header: {
+        background: 'rgba(253 198 163)',
+    },
+    home: {
+        font: 'Inter',
+        borderRadius: '50px',
+        padding: '20px',
+        fontSize: '30px',
+        color: '#EEEFEF',
+        backgroundColor: '#000000',
+    },
+    navlink:{
+        fontSize: '25px',
+        color: '#000000',
+        backgroundColor: 'rgba(253 198 163)',
+    }
+}
 
 function NavTabs() {
     const currentPage = useLocation().pathname;
 
     return (
-        <div className = "container">
+        <div className = "container" style = {styles.header}>
             <ul className = "nav nav-tabs"> 
                 <li className = "nav-item" id = "david">
-                    <Link
+                    <Link style = {styles.home}
                         to="/"
                         className={currentPage === "/" ? "nav-link active" : "nav-link"}
                     >
                     David Villarreal
                 </Link>
             </li>   
-                <li className = "nav-item" >
+                <li className = "nav-item" style = {styles.navlink}>
                     <Link
                         to="/About"
                         className={currentPage === "/About" ? "nav-link active" : "nav-link"}
@@ -24,7 +41,7 @@ function NavTabs() {
                     About Me
                     </Link>
                 </li>   
-                <li className = "nav-item">
+                <li className = "nav-item" style = {styles.navlink}>
                     <Link
                         to="/Portfolio"
                         className={currentPage === "/Portfolio" ? "nav-link active" : "nav-link"}
@@ -32,7 +49,7 @@ function NavTabs() {
                     Portfolio
                     </Link>
                 </li>   
-                <li className = "nav-item">
+                <li className = "nav-item" style = {styles.navlink}>
                     <Link
                         to="/Contact"
                         className={currentPage === "/Contact" ? "nav-link active" : "nav-link"}
@@ -40,7 +57,7 @@ function NavTabs() {
                     Contact
                     </Link>
                 </li>   
-                <li className = "nav-item">
+                <li className = "nav-item" style = {styles.navlink}>
                     <Link
                         to="/Resume"
                         className={currentPage === "/Resume" ? "nav-link active" : "nav-link"}
