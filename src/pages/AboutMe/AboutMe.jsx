958f34e0023f2888e6d2997e102a8e6d7efd4727
unknown <davidvillarr3al@gmail.com>
2025-02-10 12:23:25 -0600

    added facts for the fun facts section of the about me page

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -47,12 +47,10 @@ export default function AboutMe () {
                     <h2>Monthly subscriptions of services i provide</h2>
                     <p>Not sure which one you need? Start with Silver and upgrade at anytime!</p>
                     <p>Hover over the bullet points to learn more!</p>
-                    {/* <p>Start with Silver and upgrade at anytime!</p> */}
                     <div className='subscription-cards'>
                         <div className='subscription-card'>
                             <h2>Silver</h2>
                             <h3>$30<span>/month</span> <button>Free Trial</button></h3>
-                            {/* <button>Free Trial</button> */}
                             <ul>
                                 <li className='tooltip' data-tooltip='A one-time audit of your current website with actionable recommendations.'>Basic Website Audit</li>
                                 <li className='tooltip' data-tooltip='Pre-designed website templates.'>Access to Starter Templates</li>
@@ -66,7 +64,6 @@ export default function AboutMe () {
                         <div className='subscription-card'>
                             <h2 id='gold'>Gold</h2>
                             <h3>$60<span>/month</span></h3>
-                            {/* <button>Free Trial</button> */}
                             <ul>
                                 <li className='tooltip' data-tooltip='A one-time audit of your current website with actionable recommendations.'>Everything in the Silver tier</li>
                                 <li className='tooltip' data-tooltip='Basic updates and maintenance of your website.'>Monthly Maintenance</li>
@@ -80,7 +77,6 @@ export default function AboutMe () {
                         <div className='subscription-card'>
                             <h2 id='platinum'>Platinum</h2>
                             <h3>$100<span>/month</span></h3>
-                            {/* <button>Free Trial</button> */}
                             <ul>
                                 <li className='tooltip' data-tooltip='A one-time audit of your current website with actionable recommendations.'>Everything in the Gold tier</li>
                                 <li className='tooltip' data-tooltip='Unlimited pages with advanced functionality.(e.g. e-commerce, custom APIs'>Full Custom Website Development</li>
@@ -96,9 +92,60 @@ export default function AboutMe () {
                     <p className='note'>*Initial fee of $150 to start. Each subscription will be billed a month after the initial fee.
                     For more information on each subscription, please contact me.*</p>
                 </section>
-                {/* <section className='fun-facts'>
+                <section className='fun-facts'>
                     <h2>Fun facts about myself</h2>
-                </section> */}
+                    <h3>I love to play video games and have a few obsessions right now:</h3>
+                    <ul>
+                        <li>Smite 2 (Main: Hercules)</li>
+                        <li>Rainbow Six Siege</li>
+                        <li>Silent Hill 2</li>
+                        <li>Mortal Kombat 1 (Main: Conan The Barbarian)</li>
+                        <li>Marvel Rivals (Main: Hulk)</li>
+                    </ul>
+                    <h3>My coding environment and how I code:</h3>
+                    <ul>
+                        <li>Listening to music</li>
+                        <p>This helps me stay focused and keep my mind on the task at hand. Music will usually include
+                            songs that have a heavy focus on instruments such as TOOL. Their music tends to have a long
+                            song duration and it helps when I know I am going to be coding for about 4 hours straight.
+                        </p>
+                        <li>I get inspired by other's work</li>
+                        <p>The coding industry is full of talented individuals and I like to learn
+                            from them all the time. Although I do get references from other sites I will always do my
+                            best to understand how to code something from scratch. 
+                        </p>
+                        <li>Learning new things</li>
+                        <p>As a developer I keep up with the latest developments in the industry.
+                            I tend to follow many people who seem well established in the field
+                            to keep up with the latest trends. Someone I like to reference for JavaScript
+                            is a user named 'Ram Maheshwari' or @rammcodes_ on Instagram.
+                            This user has a great deal of posts that I find very informative and will always 
+                            add a post from them to a folder in my bookmarks and he tends to post a lot more than
+                            just JavaScript.
+                        </p>
+                    </ul>
+                        <h3>Things I like to do in my free time</h3>
+                        <ul>
+                        <li>One of my favorite things to do in my free time is
+                            to explore the city of San Antonio with my wife. We are both introverts who like to 
+                            stay inside the house as much as possible but recently we decided that we 
+                            don't want to miss out on the city that we were both raised in. 
+                        </li>
+                        <li>I like to play games with my friends in my free time and as of right now we are all
+                            very much hooked on Marvel Rivals.
+                        </li>
+                        <li>I also like to come up with website ideas for games I like to play.
+                            As of right now I am working on a website for Twisted Metal: Black and 
+                            Bioshock 2. These will be coming soon and will be posted on my portfolio!
+                        </li>
+                        <li>The lovecraftian world of horror is also something that I like to explore.
+                            I have a book that has a collection of his works and I read it whenever I have free time
+                            or when I need to take a break from coding. Although Cthulhu is my favorite character
+                            I would have to say that the story "The Nameless City" is my favorite.
+                        </li>
+
+                        </ul>
+                </section>
 
         </div>
     )
