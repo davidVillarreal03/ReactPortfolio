6d92115fc4cbe694b97470e5b2df53155bf9072b
unknown <davidvillarr3al@gmail.com>
2025-02-21 16:36:46 -0600

    scroll to top added and refresh is available for any page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Nav from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
 import SideBar from './components/SideNavBar/SideNavBar';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
 
   return (
     <>  
+      <ScrollToTop />
       <SideBar />
       {/* <Nav />  */}
       <Outlet />
